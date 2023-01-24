27e6070760b2f7774d40f6348ecd6bfbe8fff450
apm191 <apurv.mundhra@gmail.com>
2023-01-24 17:50:02 +0530

    Implemeneted Scheduler to get notification about something

--- src/main/java/com/example/demo/services/BusService.java
+++ src/main/java/com/example/demo/services/BusService.java
@@ -5,13 +5,19 @@ import com.example.demo.dao.Jpa.BusRepository;
 import com.example.demo.handler.BusServiceException;
 import com.example.demo.models.Bus;
 import jakarta.validation.Valid;
+import org.jetbrains.annotations.Async;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.Caching;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -21,6 +27,9 @@ public class BusService
     private BusRepository proRepo;
     @Autowired
     private BusRepository_JDBC proBusRepo;
+
+    Logger log = LoggerFactory.getLogger(BusService.class);
+
     @Caching(
             evict = {@CacheEvict(value = "Bus",key = "#bus.getId()")},
             put = {@CachePut(value = "Bus",key = "#bus.getId()")}
@@ -53,7 +62,12 @@ public class BusService
         }
     }
 
+    @Scheduled(cron = "0/15 * * * * *")
     public List<Bus>getBusses(){
-        return proBusRepo.getBusList();
+        List<Bus> busList = new ArrayList<>();
+        busList = proBusRepo.getBusList();
+        System.out.println("FETCH SERVICE CALL : "+ new Date().toString());
+        System.out.println("NO OF RECORDS FETCHED : "+busList.size());
+        return busList;
     }
 }
